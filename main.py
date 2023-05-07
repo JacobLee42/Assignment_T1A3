d35d2ce93544fd341144bf01a9223b455a6fa1b6
JacobLee42 <14303@coderacademy.edu.au>
2023-05-07 14:15:50 +1000

    lunchbreak

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from quiz import run_quiz, view_leaderboard
 def main_menu():
     while True:
         print("Welcome to Kapuut! The Kahoot karbon kopy multiple choice quiz game made in Python!!!")
-        print("Please choose an option:")
+        print("Please choose from one of the following options:")
         print("1) Start the Game")
         print("2) View Leaderboard")
-        print("3) Exit the said Game")
+        print("3) Exit the Game")
         choice = input("Enter your choice (1-3): ")
         if choice == "1":
             name = input("Enter your name: ")
             play_again = run_quiz(name)
             if not play_again:
-                print("Thank you for playing Kapuut! Hope to see you back soon!!!")
+                print("Thank you for playing Kapuut! Hope to see you back here real soon!!!")
             continue
         elif choice == "2":
             view_leaderboard()
