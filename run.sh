006b569fa78a3de6ad3788d3701912a139d2e1d6
JacobLee42 <14303@coderacademy.edu.au>
2023-05-14 20:55:30 +1000

    added run.sh file

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
  #!/bin/bash
 
-
+# Creates virtual environment to run packages and quiz
 python3 -m venv kapuut-venv
+# Activates the virtual environment 
 source Kapuut-venv/bin/activate
-pp3 install -r requirements.txt
+# Installs all requirements/packages needed to run game
+pip3 install -r requirements.txt
+# clears the terminal
+clear
+# Runs the main game program
 python3 main.py
