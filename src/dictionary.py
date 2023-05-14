74012356850d4a82d6c09e86303c7c60c46c3a1b
JacobLee42 <14303@coderacademy.edu.au>
2023-05-14 20:58:16 +1000

    changed folders around added, ppt,pic

---
+++ src/dictionary.py
@@ -0,0 +1,87 @@
+import string
+#moved this from main.py to dictionary.py - storing the label and answer alternatives with zip() in a dictionary...
+
+dict(zip(string.ascii_lowercase, ["He can start fires with his mind", "Super Speed", "He can turn invisible", "He can control lemmings by whistling"]))
+{'a': 'He can start fires with his mind', 'b': 'Super Speed', 'c': 'He can turn invisible', 'd': 'He can control lemmings by whistling'} 
+dict(zip(string.ascii_lowercase, ["Seth MacFarlane", "Brian Peter Green", "Seth Rogan", "Bill Burr"]))
+{'a': 'Seth MacFarlane', 'b': 'Brian Peter Green', 'c': 'Seth Rogan', 'd': 'Bill Burr'}
+dict(zip(string.ascii_lowercase, ["Seven", "One", "Nine", "Five"]))
+{'a': 'Seven', 'b': 'One', 'c': 'Nine', 'd': 'Five'}
+dict(zip(string.ascii_lowercase, ["MacGyver", "Magnum P.I.", "Die Hard", "Harry Potter"]))
+{'a': 'MacGyver', 'b': 'Magnum P.I.', 'c': 'Die Hard', 'd': 'Harry Potter'}
+dict(zip(string.ascii_lowercase, ["Tae-Jitsu", "Tae-Bo", "Fight Club", "Tang-soo-do"]))
+{'a': 'Tae-Jitsu', 'b': 'Tae-Bo', 'c': 'Fight Club', 'd': 'Tang-soo-do',}
+dict(zip(string.ascii_lowercase, ["Mila Kunis", "Meg Ryan", "Jodie Foster", "Nancy Cartwright"]))
+{'a': 'Mila Kunis', 'b': 'Meg Ryan', 'c': 'Jodie Foster', 'd': 'Nancy Cartwright'}
+dict(zip(string.ascii_lowercase, ["Golf", "Badmington", "Frisbee Golf", "Synchronised Swimming"]))
+{'a': 'Golf', 'b': 'Badmington', 'c': 'Frisbee Golf', 'd': 'Synchronised Swimming'}
+dict(zip(string.ascii_lowercase, ["It peed on the carpet", "It ran into oncoming traffic", "It attacked the mailman", "It ate all the food that was on the table for dinner",]))
+{'a': 'It peed on the carpet', 'b': 'It ran into oncoming traffic', 'c': ' It attacked the mailman', 'd': 'It ate all the food that was on the table for dinner'}
+dict(zip(string.ascii_lowercase, [ "Twiki", "Princess Fairybottom", "Heavenly Hiraani Tiger Lily Hutchence Geldof-Griffin", "Dianne Simmons Griffin",]))
+{'a': 'Twiki', 'b': 'Princess Fairybottom', 'c': 'Heavenly Hiraani Tiger Lily Hutchence Geldof-Griffin', 'd': 'Dianne Simmons Griffin'}
+dict(zip(string.ascii_lowercase, ["Bring me a toolshed, for I am hungry", "Bring me Kevin Bacon nomnomnom", "Hey you! What you lookin at!", "Hey Lois! Look at me!"]))
+{'a': 'Bring me a toolshed, for I am hungry', 'b': ' Bring me Kevin Bacon nomnomnom', 'c': 'Hey you! What you lookin at!', 'd': ' Hey Lois! Look at me!'}
+dict(zip(string.ascii_lowercase, ["86 years","17 years", "49 years", "142 years"]))
+{'a': '86 years', 'b': '17 years', 'c': '49 years', 'd': '142 years'}
+dict(zip(string.ascii_lowercase, ["A primate", "A bird", "A marsupial", "A lizard"]))
+{'a': 'A primate', 'b': 'A bird', 'c': 'A Marsupial', 'd': 'A lizard'}
+dict(zip(string.ascii_lowercase, [ "60 points", "20 points", "50 points", "100 points"]))
+{'a': '60 points', 'b': '20 points', 'c': '50 points', 'd': '100 points'}
+dict(zip(string.ascii_lowercase, ["Bear", "Dog", "Dragon", "Rabbit"])) 
+{'a': 'Bear', 'b': 'Dog', 'c': 'Dragon', 'd': 'Rabbit'}
+dict(zip(string.ascii_lowercase, ["The largest citrus fruit", "A breed of dog", "An old fashioned punching bag", "A type of hat"]))
+{'a': 'The largest citrus fruit', 'b': 'A breed of dog', 'c': 'An old fashioned punching bag', 'd': 'A type of hat'}
+dict(zip(string.ascii_lowercase, ["Han Solo", "Hannibal Lecter", "Harry Potter", "Hercules"]))
+{'a': 'Han Solo', 'b': 'Hannibal Lecter', 'c': 'Harry Potter', 'd': 'Hercules'}
+dict(zip(string.ascii_lowercase, ["Eight", "Zero", "Eleven", "Four"]))
+{'a': 'Eight', 'b': 'Zero', 'c': 'Eleven', 'd': 'Four'}
+dict(zip(string.ascii_lowercase, ["Members of Indias highest caste", "Surfers in California", "It is a totally made up word", "Hill people from the Appalachian Mountains"]))
+{'a': 'Members of Indias highest caste', 'b': 'Surfers in California', 'c': 'It is a totally made up word', 'd': 'Hill people from the Appalachian Mountains'}
+dict(zip(string.ascii_lowercase, ["Three", "Two", "Five", "Seven"]))
+{'a': 'Three', 'b': 'Two', 'c': 'Five', 'd': 'Seven'}
+dict(zip(string.ascii_lowercase, ["Bitten by a radioactive spider", "He was born with them", "Military experiment gone horribly wrong", "Woke up with them after a strange dream"]))
+{'a': 'Bitten by a radioactive spider', 'b': 'He was born with them', 'c': 'Military experiment gone horribly wrong', 'd': 'Woke up with them after a strange dream'}
+dict(zip(string.ascii_lowercase, ["Madrid", "Salvador", "Gqeberha", "Belmopan"]))
+{'a': 'Madrid', 'b': 'Salvador', 'c': 'Gqeberha', 'd': 'Belmopan'}
+dict(zip(string.ascii_lowercase, ["Rome", "Ankara", "Gyros", "Reggiano"]))
+{'a': 'Rome', 'b': 'Ankara', 'c': 'Gyros', 'd': 'Reggiano'}
+dict(zip(string.ascii_lowercase, ["Berlin", "Bruge", "Bandung", "Brassels"]))
+{'a': 'Berlin', 'b': 'Bruge', 'c': 'Bandung', 'd': 'Brassels'}
+dict(zip(string.ascii_lowercase, ["Ottawa", "Toronto", "Winnipeg", "Montreal"]))
+{'a': 'Ottawa', 'b': 'Toronto', 'c': 'Winnipeg', 'd': 'Montreal'}
+dict(zip(string.ascii_lowercase, ["Beirut", "Durban", "Gaborone", "Lagos"]))
+{'a': 'Beirut', 'b': 'Durban', 'c': 'Gaborone', 'd': 'Lagos'}
+dict(zip(string.ascii_lowercase, ["Buenos Aires", "Nelson", "Brasilia", "Dodoma"]))
+{'a': 'Buenos Aires', 'b': 'Nelson', 'c': 'Brasilia', 'd': 'Dodoma'}
+dict(zip(string.ascii_lowercase, ["Brasilia", "Dodoma", "Mombasa", "Doha"]))
+{'a': 'Brasilia', 'b': 'Dodoma', 'c': 'Mombasa', 'd': 'Doha'}
+dict(zip(string.ascii_lowercase, ["Bogota", "Mandalay", "Bern", "Edirne"]))
+{'a': 'Bogota', 'b': 'Mandalay', 'c': 'Bern', 'd': 'Erdine'}
+dict(zip(string.ascii_lowercase, ["Pyongyang", "Bloemfontein", "Port Harcourt", "Gyeongbokgung"]))
+{'a': 'Pyongyang', 'b': 'Bloemfontein', 'c': 'Port Harcourt', 'd': 'Gyeongbokgung'}
+dict(zip(string.ascii_lowercase, ["Nairobi", "Kisumu", "Namibia", "Georgetown"]))
+{'a': 'Nairobi', 'b': 'Kisumu', 'c': 'Namibia', 'd': 'Georgetown'}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/questions.py
@@ -0,0 +1,155 @@
+questions_dict = {
+    "When the family became the Super Griffins, what was Chris's superpower?": {
+        "alternatives": ["He can start fires with his mind", "Super Speed", "He can turn invisible", "He can control lemmings by whistling"],
+        "correct_answer": "He can start fires with his mind"
+        
+
+    },
+    "Who provides the voice of Peter Griffin?": {
+        "alternatives": ["Seth MacFarlane", "Brian Peter Green", "Seth Rogan", "Bill Burr"],
+        "correct_answer": "Seth MacFarlane"
+        
+
+    },
+    "How old is Brian the dog in dog years?": {
+        "alternatives": ["Seven", "One", "Nine", "Five"],
+        "correct_answer": "Seven"
+    },
+    "Fill in the blank on this letter written by Peter; Dear_____, enclosed is a rubber band, a paper clip and a drinking straw. Please save my dog.": {
+        "alternatives": ["MacGyver", "Magnum P.I.", "Die Hard", "Harry Potter"],
+        "correct_answer": "Macgyver"
+
+    },
+    "Peter begins exploiting Lois's fighting ability after she learns what fictional martial art?": {
+        "alternatives": ["Tae-Jitsu", "Tae-Bo", "Fight Club", "Tang-soo-do"],
+        "correct_answer": "Tae-Jitsu"
+
+    },
+    "Who provides the voice of Meg Griffin?": {
+        "alternatives": ["Mila Kunis","Meg Ryan", "Jodie Foster", "Nancy Cartwright"],
+        "correct_answer": "Mila Kunis"
+
+    },
+    "Peter's great great uncle Agnus Griffin invented what sport?": {
+        "alternatives": ["Golf", "Badmington", "Frisbee Golf", "Synchronised Swimming"],
+        "correct_answer": "Golf"
+
+    },
+    "What did Peter and Lois's pet rock do the first day they took it home?": {
+        "alternatives": ["It peed on the carpet", "It ran into oncoming traffic", "It attacked the mailman", "It ate all the food that was on the table for dinner"],
+        "correct_answer": "It peed on the carpet"
+
+    },
+    "What did Peter want to name Meg when she was born?": {
+        "alternatives": ["Twiki", "Princess Fairybottom", "Heavenly Hiraani Tiger Lily Hutchence Geldof-Griffin", "Dianne Simmons Griffin"],
+        "correct_answer": "Twiki"
+
+    },
+    "When Peter turns the house into a puppet, what does it say?": {
+        "alternatives": ["Bring me a toolshed, for I am hungry", "Bring me Kevin Bacon nomnomnom", "Hey you! What you lookin at!", "Hey Lois! Look at me!"],
+        "correct_answer": "Bring me a toolshed, for I am hungry"
+    #General knowledge quiz questions and multiple choice answers
+    },
+    "What is age is the longest recorded age that an elephant has ever lived?": {
+        "alternatives": ["86 years", "17 years", "49 years", "142 years"],
+        "correct_answer": "86 years"
+
+    },
+    "What is a Tarsier?": {
+        "alternatives": ["A primate","A bird", "A marsupial", "A lizard"],
+        "correct_answer": "A primate"
+
+    },
+    "In darts, what's the most points you can score with a single throw?": {
+       "alternatives": ["60 points", "20 points", "50 points", "100 points"],
+       "correct_answer": "60 points"
+
+    },
+    "Which of these animals does NOT appear in the Chinese zodiac?": {
+        "alternatives": ["Bear", "Dog", "Dragon", "Rabbit"],
+        "correct_answer": "Bear"
+
+    },
+    "What is a Pomelo?": {
+        "alternatives": ["The largest citrus fruit", "A breed of dog", "An old fashioned punching bag", "A type of hat"],
+        "correct_answer": "The largest citrus fruit"
+        
+
+    },
+    "Who killed Greedo?": {
+        "alternatives": ["Han Solo", "Hannibal Lecter", "Harry Potter", "Hercules"],
+        "correct_answer": "Han Solo"
+
+    },
+    "How many points is the letter X worth in Scrabble?": {
+        "alternatives": ["Eight", "Zero", "Eleven", "Four"],
+        "correct_answer": "Eight"
+
+    },
+    "Who are known as Brahmins?": {
+        "alternatives": ["Members of India's highest caste", "Surfers in California", "It's a totally made up word", "Hill people from the Appalachian Mountains"],
+        "correct_answer": "Members of India's highest caste"
+
+    },
+    "How many holes are on a standard bowling ball?": {
+        "alternatives": ["Three", "Two", "Five", "Seven"],
+        "correct_answer": "Three"
+
+    },
+    "How did Spider-man get his powers?": {
+        "alternatives": ["Bitten by a radioactive spider", "He was born with them", "Military experiment gone horribly wrong", "Woke up with them after a strange dream"],
+        "correct_answer": "Bitten by a radioactive spider"
+
+    #Capital Cities of the World questions and Multiple choice answers
+    },
+    "What is the capital of Spain?": {
+        "alternatives": ["Madrid", "Salvador", "Gqeberha", "Belmopan"],
+        "correct_answer": "Madrid"
+
+    },
+    "What is the capital of Italy?": {
+        "alternatives": ["Rome", "Ankara", "Gyros", "Reggiano"],
+        "correct_answer": "Rome"
+
+    },
+    "What is the capital of Germany?": {
+       "alternatives": ["Berlin", "Bruge", "Bandung", "Brassels"],
+       "correct_answer": "Berlin"
+
+    },
+    "What is the capital of Canada?": {
+        "alternatives": ["Ottawa", "Toronto", "Winnipeg", "Montreal"],
+        "correct_answer": "Ottawa"
+
+    },
+    "What is the capital of Lebanon?": {
+        "alternatives": ["Beirut", "Durban", "Gaborone", "Lagos"],
+        "correct_answer": "Beirut"
+
+    },
+    "What is the capital of Argentina?": {
+        "alternatives": ["Buenos Aires", "Nelson", "Brasilia", "Dodoma"],
+        "correct_answer": "Buenos Aires"
+
+    },
+    "What is the capital of Brazil?": {
+        "alternatives": ["Brasilia", "Dodoma", "Mombasa", "Doha"],
+        "correct_answer": "Brasilia"
+
+    },
+    "What is the capital of Colombia?": {
+        "alternatives": ["Bogota", "Mandalay", "Bern", "Edirne"],
+        "correct_answer": "Bogota"
+
+    },
+    "What is the capital of North Korea?": {
+        "alternatives": ["Pyongyang", "Bloemfontein", "Port Harcourt", "Gyeongbokgung"],
+        "correct_answer": "Pyongyang"
+
+    },
+    "What is the capital of Kenya?": {
+        "alternatives": ["Nairobi", "Kisumu", "Namibia", "Georgetown"],
+        "correct_answer": "Nairobi"
+
+    },
+} 
